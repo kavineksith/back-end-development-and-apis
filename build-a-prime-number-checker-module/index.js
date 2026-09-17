9e538ccdcc0fd34e2d2a3438b9fa80a99fc725ed
Kavin Eksith <143245901+kavineksith@users.noreply.github.com>
2026-09-17 18:45:31 +0000

    Complete prime number checker module

---
+++ build-a-prime-number-checker-module/index.js
@@ -0,0 +1,13 @@
+function isPrime(num) {
+  if (!Number.isInteger(num) || num < 2) return false;
+  if (num === 2) return true;
+  if (num % 2 === 0) return false;
+
+  for (let i = 3; i * i <= num; i += 2) {
+    if (num % i === 0) return false;
+  }
+  return true;
+}
+
+module.exports = isPrime;
+module.exports.isPrime = isPrime;
